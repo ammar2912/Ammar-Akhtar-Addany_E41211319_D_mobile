f780e65bafabbf75f7ea8d1ec83197ca1804ea13
Ammar <akhtarammar29@gmail.com>
2023-03-14 14:54:06 +0700

    tugas 5

--- minggu5/acara23/app/src/main/java/com/example/acara23/SecondActivity.java
+++ minggu5/acara23/app/src/main/java/com/example/acara23/SecondActivity.java
@@ -1,2 +1,22 @@
-package com.example.acara23;public class SecondActivity {
+package com.example.acara23;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView txtHello;
+    private String nama;
+    private String KEY_NAME = "NAMA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        txtHello = (TextView) findViewById(R.id.txtHello);
+        Bundle extras = getIntent().getExtras();
+        nama = extras.getString(KEY_NAME);
+        txtHello.setText("Hello, " + nama + " !");
+    }
 }
